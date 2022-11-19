a523ec5afe3a468d4daae9c7fac939107a96c820
unknown <alaminkhan6203@gmail.com>
2022-11-19 12:05:00 +0600

    fixing bugs and replacing with tailwind

--- Components/Tweet.tsx
+++ Components/Tweet.tsx
@@ -54,14 +54,14 @@ const Tweet=({tweet,showTime,showMetrics,showSource}:any)=> {
   return (
     <div className='p-1 md:p-1.5 lg:p-2 rounded-sm' >
 			<div className='tweet'>
-	            <div className="user">
+	            <div className="flex items-center">
 	            	<Image src={dp} className="w-[55px] md:w-[58px] lg:w-[60px] rounded-full" />
-	            	<div className="names">
-	            		<p  className='name text-base md:text-lg lg:text-xl'>
-            				<span className="bold n">{name}</span>
+	            	<div className="ml-4 flex-1">
+	            		<p  className='flex text-base md:text-lg lg:text-xl'>
+            				<span className="bold mr-2">{name}</span>
 	            			{isVerified && <svg style={{color: '#1DA1F2'}} viewBox="0 0 24 24" aria-label="Verified account" fill="currentColor" focusable="false" width='16'><g><path d="M22.5 12.5c0-1.58-.875-2.95-2.148-3.6.154-.435.238-.905.238-1.4 0-2.21-1.71-3.998-3.818-3.998-.47 0-.92.084-1.336.25C14.818 2.415 13.51 1.5 12 1.5s-2.816.917-3.437 2.25c-.415-.165-.866-.25-1.336-.25-2.11 0-3.818 1.79-3.818 4 0 .494.083.964.237 1.4-1.272.65-2.147 2.018-2.147 3.6 0 1.495.782 2.798 1.942 3.486-.02.17-.032.34-.032.514 0 2.21 1.708 4 3.818 4 .47 0 .92-.086 1.335-.25.62 1.334 1.926 2.25 3.437 2.25 1.512 0 2.818-.916 3.437-2.25.415.163.865.248 1.336.248 2.11 0 3.818-1.79 3.818-4 0-.174-.012-.344-.033-.513 1.158-.687 1.943-1.99 1.943-3.484zm-6.616-3.334l-4.334 6.5c-.145.217-.382.334-.625.334-.143 0-.288-.04-.416-.126l-.115-.094-2.415-2.415c-.293-.293-.293-.768 0-1.06s.768-.294 1.06 0l1.77 1.767 3.825-5.74c.23-.345.696-.436 1.04-.207.346.23.44.696.21 1.04z"></path></g></svg>}
 	            		</p>
-	            		<p  className='sec text-base md:text-lg lg:text-xl'>
+	            		<p  className='text-gray-500 text-base md:text-lg lg:text-xl'>
 	            		 	@{username}
 	            		</p>
 	            	</div>
@@ -70,13 +70,13 @@ const Tweet=({tweet,showTime,showMetrics,showSource}:any)=> {
 	           		{text}
 	            </p>
 	            {image && <Image src={image} className="mt-2" objectFit='cover'/>}
-	            <div className='time_source sec'>
+	            <div className='flex items-center mt-1 text-gray-500'>
             		<p className='text-sm md:text-base lg:text-lg'>
             			<span>{showTime && date && format(date, 'h:mm a · LLL d, yyyy · ')}</span>
             			{showSource && <span>{source}</span>}
             		</p>
 	            </div>
-	            {showMetrics && <div className='metrics'>
+	            {showMetrics && <div className='flex mt-1'>
                 	<p className='text-sm md:text-base lg:text-lg'>
                 		<span className='bold'>{modLikes}</span> <span className='sec likes'> Likes</span>  <span className='bold'>{modRetweets}</span> <span className='sec'> Retweets</span>
                 	</p>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,19 @@ import axios from 'axios'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import domtoimage from 'dom-to-image'
+import { saveAs } from 'file-saver'
 import { useRef, useState } from 'react'
 import Header from '../Components/Header'
 import Main from '../Components/Main'
+import Settings from '../Components/Settings'
 import styles from '../styles/Home.module.css'
 
 
 export default function Home() {
   const [bg, setBg] = useState('linear-gradient(106.8deg, rgb(117, 255, 220) 6%, rgb(163, 216, 255) 47.6%, rgb(248, 215, 251) 87.8%)')
 
-  const tweetRef = useRef(null)
+  const tweetRef = useRef<HTMLButtonElement |null>(null)
 
   const [tweetData, setTweetData] = useState(null)
 
@@ -24,7 +27,7 @@ export default function Home() {
   const [hint, setHint] = useState(true)
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(false)
-  const bringTweet= async(e:React.SyntheticEvent)=>{
+  const bringTweet= async(e:React.FormEventHandler<HTMLFormElement>)=>{
     try{
       e.preventDefault()
       setHint(false)
@@ -49,6 +52,52 @@ export default function Home() {
       setTweetData(null)
     }
   }
+
+  const convert = async (format:any) => {
+
+    const node = tweetRef.current
+    const scale = 2
+
+    let dataUrl
+
+    const style = {
+        transform: 'scale(2)',
+        transformOrigin: 'top left',
+    }
+
+    const param = {
+       height: node.offsetHeight * scale,
+       width: node.offsetWidth * scale,
+       quality: 1,
+       style
+    }
+
+    switch (format) {
+        case 'png':
+            {
+                dataUrl = await domtoimage.toPng(node, param)
+                saveAs(dataUrl, `${new Date().toJSON()}.${format}`)
+                return
+            }
+
+        case 'jpeg':
+            {
+                dataUrl = await domtoimage.toJpeg(node, param)
+                saveAs(dataUrl, `${new Date().toJSON()}.${format}`)
+                return
+            }
+
+        case 'svg':
+            {
+                dataUrl = await domtoimage.toSvg(node, param)
+                saveAs(dataUrl, `${new Date().toJSON()}.${format}`)
+                return
+            }
+    }
+}
+const propsForSettings = {
+  showTime, setShowTime, showMetrics, setShowMetrics, showSource, setShowSource, scale, setScale, convert, bg, setBg
+}
   return (
     <>
         <Head>
@@ -57,9 +106,10 @@ export default function Home() {
             </Head >
 
             
-<Header bringTweet={bringTweet}/>
+        <Header bringTweet={bringTweet}/>
             <div>
-<Main  tweetRef={tweetRef} bg={bg} scale={scale} hint={hint} loading={loading} error={error} tweetData={tweetData} showTime={showTime} showMetrics={showMetrics} showSource={showSource} />
+        <Main  tweetRef={tweetRef} bg={bg} scale={scale} hint={hint} loading={loading} error={error} tweetData={tweetData} showTime={showTime} showMetrics={showMetrics} showSource={showSource} />
+            {!hint && <Settings props={propsForSettings}/>}
             </div>
             <footer>
               <p className="fontInter">Check out the sourcecode of this project at my <Link href="https://github.com/AlAminKh03"><a className="text-green-500"> GitHub</a></Link></p>
